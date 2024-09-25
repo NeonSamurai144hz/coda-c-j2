c3742b9bccd5c1a1511de7525a5bf9db96bce6b9
Bobeica Nikita <nbobeica@DESKTOP-44P6421>
2024-09-25 14:23:35 +0200

    exercice 4

---
+++ ex-4/main.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+        int x = 30;
+        int y = 15;
+        int resultat = x * y;
+	printf("%d x %d = %d\n", x, y, resultat);
+	exit(0);
+}
