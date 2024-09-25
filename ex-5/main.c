5d0ab0cb018d18e0469d1e383e2186e045403a21
Bobeica Nikita <nbobeica@DESKTOP-44P6421>
2024-09-25 14:35:46 +0200

    exercice 5

---
+++ ex-5/main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+        int x = 12;
+        int y = 7;
+        int resultat = (x + y) / 2;
+        printf("La moyenne de 12 et 7 est égale à %.2d\n", resultat);
+        exit(0);
+}
+
